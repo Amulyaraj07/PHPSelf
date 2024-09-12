2aadcde860f563d52e04051f449fae1bb89f1c0e
Amulya Raj <amulyaraj1234@gmail.com>
2024-09-12 15:11:56 +0000

    added comments

--- logicalOperator.php
+++ logicalOperator.php
@@ -3,19 +3,19 @@
  $y=20;
  $z=10;
 
- if($x==10&&$y==20){
+ if($x==10&&$y==20){ //and operator
     echo "True";
  }
 
- if($x>10||$y>10){
+ if($x>10||$y>10){//or operator
     echo "True";
  }
 
- if($x>10 xor $z==10){
+ if($x>10 xor $z==10){//xor operator
     echo "True";
  }
 
- echo "!$x";
+ echo "!$x";//not operator
 
 
 

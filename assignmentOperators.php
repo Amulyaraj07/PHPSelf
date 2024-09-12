5a2fe744e0b2f27b89e63f904a5b7ab34d8f126c
Amulya Raj <amulyaraj1234@gmail.com>
2024-09-12 15:08:04 +0000

    added comments

--- assignmentOperators.php
+++ assignmentOperators.php
@@ -8,7 +8,7 @@
     echo $a+=$b; //add the value and assign back the final value to a
     echo $a-=$b;//subtracts the value and assign back the final value to a 
     echo $a*=$b;//multiplies the value and assign back the final value to a 
-    echo $a/=$b;
+    echo $a/=$b;//divides the value and assign back the final value to a 
     echo $a%=$b;
     echo $a**=$b;
 

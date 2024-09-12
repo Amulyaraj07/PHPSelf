0a23ca65f7d04c1ace43d61b630af9439b855355
Amulya Raj <amulyaraj1234@gmail.com>
2024-09-12 15:09:40 +0000

    added comments

--- assignmentOperators.php
+++ assignmentOperators.php
@@ -10,7 +10,7 @@
     echo $a*=$b;//multiplies the value and assign back the final value to a 
     echo $a/=$b;//divides the value and assign back the final value to a 
     echo $a%=$b;//perform modulo on the value and assign back the final value to a 
-    echo $a**=$b;
+    echo $a**=$b;//power the values a to the power b  and assign back the final value to a 
 
 
 
